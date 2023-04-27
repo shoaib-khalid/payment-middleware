9b3b143cc319f0597338c0d808c08a1fa7efc286
Wafi <ahmad.wafi@kalsym.com>
2023-04-27 11:59:29 +0800

    Add redirect

--- src/app/modules/landing/thankyou/thankyou.component.ts
+++ src/app/modules/landing/thankyou/thankyou.component.ts
@@ -1,7 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { finalize, Subject, takeUntil, takeWhile, tap, timer } from 'rxjs';
+import { finalize, interval, Subject, takeUntil, takeWhile, tap, timer } from 'rxjs';
 import { AppConfig } from "app/config/service.config";
 
 @Component({
@@ -17,6 +17,12 @@ export class ThankYouComponent
         paymentType     : string;
         status          : string;
     }
+    channel : string = '';
+    countdown : number = 5;
+    countdownMapping: any = {
+        '=1'   : '# second',
+        'other': '# seconds'
+    };
 
     private _unsubscribeAll: Subject<any> = new Subject<any>();
 
@@ -29,7 +35,8 @@ export class ThankYouComponent
         private _apiServer: AppConfig
     )
     {        
-        this.params = this._activatedRoute.snapshot.paramMap['params']        
+        this.params = this._activatedRoute.snapshot.paramMap['params'];
+        this.channel = this._activatedRoute.snapshot.queryParams['channel'] ?? '';                
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -41,6 +48,29 @@ export class ThankYouComponent
      */
     ngOnInit(): void
     {
+        // Redirect after the countdown
+        interval(1000)
+            .pipe(
+                finalize(() => {
+
+                    if (this.channel === 'DELIVERIN') {
+                        this._document.location.href = 'https://' + this._apiServer.settings.marketplaceDomain;
+                    }
+                    
+                    // if (this.params.serviceType === 'DINEIN')
+                    // {
+                    //     this._document.location.href = 'https://' + this._apiServer.settings.dineInDomain + '/order-history';
+                    // }
+                    // else if (this.params.serviceType === 'DELIVERIN')
+                    // {
+                    //     this._document.location.href = 'https://' + this._apiServer.settings.marketplaceDomain;
+                    // }
+                }),
+                takeWhile(() => this.countdown > 0),
+                takeUntil(this._unsubscribeAll),
+                tap(() => this.countdown--)
+            )
+            .subscribe();
     }
 
     /**
@@ -51,6 +81,18 @@ export class ThankYouComponent
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
+    }
+
+    redirect()
+    {
+        switch (this.channel) {
+            case 'DELIVERIN':
+                this._document.location.href = 'https://' + this._apiServer.settings.marketplaceDomain;
+                break;
+                
+            default:
+                break;
+        }
     }
 
 }
